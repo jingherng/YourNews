9e81df162f9b23ec27dfee954d34da12a43022ec
jh laptop <jingherng2010@gmail.com>
2018-04-04 00:22:25 +0800

    Added AI recommends. Fixed some bugs

---
+++ app/src/main/java/com/example/powjh/yournews/AIrecommends.java
@@ -0,0 +1,14 @@
+package com.example.powjh.yournews;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AIrecommends extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airecommends);
+    }
+
+}
---
+++ app/src/main/java/com/example/powjh/yournews/watsonFrag.java
@@ -0,0 +1,8 @@
+package com.example.powjh.yournews;
+
+/**
+ * Created by Pow JH on 03-Apr-18.
+ */
+
+public class watsonFrag {
+}
